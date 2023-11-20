4f94c2dd96211885720a706554ae1787ef844a2f
YutoKikuta <ykikutal@students.cs.ubc.ca address>
2023-11-20 15:24:15 -0800

    Phase3

--- src/test/model/StatisticsTest.java
+++ src/test/model/StatisticsTest.java
@@ -2,13 +2,19 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class StatisticsTest {
     private Statistics testStat;
+    private Task task1;
+    private Task task2;
 
     @BeforeEach
     void runBefore() {
         testStat = new Statistics();
+        task1 = new Task("Task 1");
+        task2 = new Task("Task 2");
     }
 
     @Test
@@ -43,4 +49,26 @@ public class StatisticsTest {
         testStat.addTotalWorkTime(0);
         assertEquals(20, testStat.getTotalWorkTime());
     }
+
+    @Test
+    void testAddCompletedTaskList() {
+        testStat.addCompletedTaskList(task1);
+        assertEquals(1, testStat.getCompletedTaskSize());
+
+        testStat.addCompletedTaskList(task2);
+        assertEquals(2, testStat.getCompletedTaskSize());
+    }
+
+    @Test
+    void testGetCompletedTaskList() {
+        testStat.addCompletedTaskList(task1);
+        testStat.addCompletedTaskList(task2);
+
+        List<Task> tasks = testStat.getCompletedTaskList();
+        assertNotNull(tasks);
+        assertEquals(2, tasks.size());
+        assertTrue(tasks.contains(task1));
+        assertTrue(tasks.contains(task2));
+    }
+
 }
