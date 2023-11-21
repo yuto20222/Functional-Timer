2ef15fd2e3c572a538a7fb703e97f4fee209f5b2
YutoKikuta <ykikutal@students.cs.ubc.ca address>
2023-11-20 16:28:37 -0800

    Phase3

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,11 @@ package persistence;
 
 import org.json.JSONObject;
 
+/**
+ * Represents a contract for objects that can be written to JSON.
+ * Classes implementing this interface should provide their own implementation
+ * of the toJson method to convert their state to a JSONObject.
+ */
 public interface Writable {
     JSONObject toJson();
 }
